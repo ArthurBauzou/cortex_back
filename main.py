5080932d1ffec0cf70dfa07661660c2957a49b54
Arthur BAUZOU <arthurbauzou@gmail.com>
2023-12-15 15:17:00 +0100

    requirements

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from db.database import testread
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 async def home():
@@ -10,5 +19,5 @@ async def home():
 @app.get('/test')
 async def test():
     blankman = testread()
-    print(blankman['name'])
+    # print(blankman['name'])
     return blankman
